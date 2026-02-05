0251abd5b459c81def2ccaef7f3fe9f56c93e6de
hirakinii <hiraki@nii.ac.jp>
2026-02-05 16:31:02 +0900

    feat: add core types

---
+++ src/types/user.ts
@@ -0,0 +1,45 @@
+export interface OsfUser {
+    id: string;
+    type: 'users';
+    attributes: {
+        fullName: string;
+        givenName: string;
+        middleNames: string;
+        familyName: string;
+        suffix: string;
+        dateRegistered: string;
+        active: boolean;
+        timezone?: string;
+        locale?: string;
+        social?: Record<string, string | string[]>;
+        employment?: Array<{
+            institution: string;
+            title?: string;
+            department?: string;
+            startYear?: number;
+            startMonth?: number;
+            endYear?: number;
+            endMonth?: number;
+            ongoing: boolean;
+        }>;
+        education?: Array<{
+            institution: string;
+            degree?: string;
+            department?: string;
+            startYear?: number;
+            startMonth?: number;
+            endYear?: number;
+            endMonth?: number;
+            ongoing: boolean;
+        }>;
+    };
+    relationships?: {
+        nodes?: { links: { related: { href: string } } };
+        institutions?: { links: { related: { href: string } } };
+    };
+    links: {
+        self: string;
+        html: string;
+        profile_image?: string;
+    };
+}
